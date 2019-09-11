ca4c608ba2e7f03d8c846bbd20e5d95345ef150e
lianhong <lianhong@mama.cn>
2019-09-11 09:44:30 +0800

    master springboot-demo-actuator 在SpringBoot中通过actuator检查项目运行情况

---
+++ springboot-demo-actuator/src/main/java/com/andy/actuator/SpringBootDemoActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.andy.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lianhong
+ * @description
+ * @date 2019/9/10 0010下午 8:10
+ */
+@SpringBootApplication
+public class SpringBootDemoActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoActuatorApplication.class,args);
+    }
+}
