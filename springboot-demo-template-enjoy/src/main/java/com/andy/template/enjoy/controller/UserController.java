9e3293a096a05baf2c90cbefbe336c3583d64868
lianhong <lianhong@mama.cn>
2019-09-18 21:55:47 +0800

    master spring-boot-demo-template-enjoy springboot 整合enjoy模板引擎

---
+++ springboot-demo-template-enjoy/src/main/java/com/andy/template/enjoy/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.andy.template.enjoy.controller;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.andy.template.enjoy.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author lianhong
+ * @description
+ * @date 2019/9/18 0018下午 7:39
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping("/login")
+    public ModelAndView login(HttpServletRequest request, User user) {
+        ModelAndView mv = new ModelAndView();
+        if (ObjectUtil.isNull(user)) {
+            mv.setViewName("redirect:page/login");
+        } else {
+            mv.addObject(user);
+            mv.setViewName("redirect:/");
+        }
+
+        request.getSession().setAttribute("user",user);
+
+        return mv;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("page/login");
+    }
+}
---
+++ springboot-demo-template-enjoy/src/main/java/com/andy/template/enjoy/model/User.java
@@ -0,0 +1,14 @@
+package com.andy.template.enjoy.model;
+
+import lombok.Data;
+
+/**
+ * @author lianhong
+ * @description
+ * @date 2019/9/18 0018下午 7:38
+ */
+@Data
+public class User {
+    private String name;
+    private String password;
+}
